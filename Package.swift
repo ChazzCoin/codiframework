4c484721d459b2a1beff589b3be09a3485719034
Chazzcoin <chazzcoin@gmail.com>
2023-12-22 01:55:28 -0600

    initial commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CodiFramework",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CodiFramework",
+            targets: ["CodiFramework"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/realm/realm-cocoa.git", from: "10.0.0"), // Replace "10.0.0" with the version you require
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CodiFramework"),
+        .testTarget(
+            name: "CodiFrameworkTests",
+            dependencies: ["CodiFramework"]),
+    ]
+)
