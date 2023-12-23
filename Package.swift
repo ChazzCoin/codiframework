f40217645b04f0edfeec9893ed5c01a0ec80544b
Chazzcoin <chazzcoin@gmail.com>
2023-12-22 23:01:16 -0600

    restructure

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "CodiFramework",
-            targets: ["CodiFramework"]),
+            name: "Codi",
+            targets: ["Codi"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on. github.com/firebase/firebase-ios-sdk.git
@@ -24,13 +24,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CodiFramework",
+            name: "Codi",
             dependencies: [
                 .product(name: "RealmSwift", package: "realm-cocoa"),
                 .product(name: "FirebaseDatabase", package: "firebase-ios-sdk")
             ]),
-        .testTarget(
-            name: "CodiFrameworkTests",
-            dependencies: ["CodiFramework"]),
     ]
 )
